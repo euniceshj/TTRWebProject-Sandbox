6678118f772aaacfb13000f8aec0c0a1f92be7d8
Stephen <lyz_mj@hotmail.com>
2022-06-17 11:03:43 +0800

    edited security to include codes

--- src/main/java/org/generation/WebProjectTTR/Security/MvcConfig.java
+++ src/main/java/org/generation/WebProjectTTR/Security/MvcConfig.java
@@ -1,10 +1,9 @@
 package org.generation.WebProjectTTR.Security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
-
-
-
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
